29ac5fc12a4448d145da3d8272572e0da4ff595f
Marcus Winding <cufbas@gmail.com>
2024-12-08 20:13:22 +0100

    added amp

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -6,6 +6,7 @@ from models.unet import ScoreNetwork0 as UNet
 from sampler.sampler import Sampler
 from sampler.image_generator import ImageGenerator
 from utils.image_saver import ImageSaver
+from torch.cuda.amp import GradScaler
 import os
 
 
@@ -28,6 +29,7 @@ class Trainer(nn.Module):
         self.save_frequency = 100
         self.clip_value = 1.0
         self.use_wandb = getattr(config, 'use_wandb', False)
+        self.scaler = GradScaler()
         print(self.use_wandb)
 
 
@@ -44,8 +46,7 @@ class Trainer(nn.Module):
     def compute_loss(self, gen_noise, predicted_noise):
         return self.loss_fn(gen_noise, predicted_noise)
 
-    def train_step(self, image, batch_idx):
-
+    def train_step(self, image):
         image = image.to(self.config.device)
         t = self.sampler.sample_time_step()
         t = t.to(self.config.device)
@@ -54,32 +55,24 @@ class Trainer(nn.Module):
         eps = torch.randn_like(image, device=self.config.device)  
         img_noise, gen_noise = self.image_generator.sample_img_at_t(t, image, alpha_bar, eps)
 
-        #img_noise  = img_noise.view(img_noise.shape[0], -1)
-        pred_noise = self.unet(img_noise, t)
-        #gen_noise = gen_noise.view(gen_noise.shape[0], -1)
-        loss = self.compute_loss(pred_noise, gen_noise)
+        with torch.autocast(device_type=self.config.device, dtype=torch.float16):
+            pred_noise = self.unet(img_noise, t)
+            loss = self.compute_loss(pred_noise, gen_noise)
 
         self.optimizer.zero_grad()
-        loss.backward()
+        self.scaler.scale(loss).backward()
 
-       # print(f"Batch {batch_idx}, Gradients:")
-       # for name, param in self.unet.named_parameters():
-        #    if param.grad is not None:
-         #       print(f"{name}: mean={param.grad.abs().mean().item():.6f}, "
-          #              f"max={param.grad.abs().max().item():.6f}, "
-           #             f"min={param.grad.abs().min().item():.6f}")
-            #else:
-             #   print(f"{name}: grad=None")
-
-        # Gradient clipping
-  
-  
-  
+        self.scaler.unscale_(self.optimizer)
         torch.nn.utils.clip_grad_norm_(self.unet.parameters(), max_norm=self.clip_value)
-        self.optimizer.step()
+        
+        self.scaler.step(self.optimizer)
+        self.scaler.update()
+
         return loss.item()
 
+
     def train(self, data_loader, num_epochs):
+
         # Reconfigure the dataloader for optimal GPU transfer
         data_loader = DataLoader(
             data_loader.dataset,
@@ -92,6 +85,7 @@ class Trainer(nn.Module):
         self.config.num_epochs = num_epochs
         self.config.batch_size = data_loader.batch_size
         best_model_loss = 10
+
         for epoch in range(num_epochs):
             epoch_loss = 0.0
             for batch_idx, batch in enumerate(data_loader):
