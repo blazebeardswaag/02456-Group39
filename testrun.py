7d9bd51cc04f383c9824811695cf1941fc0158e5
Marcus Winding <cufbas@gmail.com>
2024-12-11 18:18:49 +0100

    lowered time emb dim

--- testrun.py
+++ testrun.py
@@ -30,7 +30,7 @@ with context_manager(
         "down_channels": [32, 64, 128, 256],  
         "mid_channels": [256, 256, 128], 
         "down_sample": [True, True, False],      
-        "time_emb_dim": 256,              
+        "time_emb_dim": 128,              
         "num_down_layers": 4,             
         "num_mid_layers": 2,              
         "num_up_layers": 4,              
